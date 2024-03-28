7e84ef3a4847527f68f567669edb420180806b0e
没有失去梦想的咸鱼 <37861076+MiaoHaoSunny@users.noreply.github.com>
2024-03-28 09:49:27 +0100

    Update train_PEMSD408.py

--- train/train_PEMSD408.py
+++ train/train_PEMSD408.py
@@ -164,7 +164,7 @@ def train_PEMSD408(args):
                 his_loss.append(mvalid_mae)
                 log = 'Epoch: {:03d}, Train Loss: {:.4f}, Train MAE: {:.4f} Train MAPE: {:.4f}, Train RMSE: {:.4f}, Valid Loss: {:.4f}, Valid MAPE: {:.4f}, Valid RMSE: {:.4f}, Training Time: {:.4f}/epoch'
                 logger.info(log.format(i, mtrain_loss, mtrain_mae, mtrain_mape, mtrain_rmse, mvalid_mae, mvalid_mape, mvalid_rmse, (t2-t1)))
-                
+                torch.save(model.state_dict(), args.save+"_epoch_"+str(i)+"_"+str(round(mvalid_mae,2))+'_task' + str(task_id)+".pth")
             logger.info('Average Training Time: {:.4f} sec/epoch'.format(np.mean(train_time)))
             logger.info('Average Inferecen Time: {:.4f} sec'.format(np.mean(val_time)))
 
